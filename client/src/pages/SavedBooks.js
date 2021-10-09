533f54205bf2f06a6acdb6243fb7ee7365598d5c
JasonDesktop <MaJason93@gmail.com>
2021-10-09 16:48:49 -0700

    Saved Savedbooks. Need to test

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
-
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import '../index.css';
