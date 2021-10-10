78bbba6a90f1a08406aa2642e3f24f2bc7ee8ca0
JasonDesktop <MaJason93@gmail.com>
2021-10-10 14:59:50 -0700

    Another Heroku Test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/still-springs-38637', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksserachmen', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
