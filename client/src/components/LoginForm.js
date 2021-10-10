1aaf30870c7aac5215dc4e6bca82699fa41c058d
JasonDesktop <MaJason93@gmail.com>
2021-10-10 09:35:45 -0700

    Reverted to previous build before HEROKU broke everything

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 // import { loginUser } from '../utils/API';
-import {LOGIN_USER} from '../utils/mutations'
-import { useMutation } from '@apollo/react-hooks'
+import {LOGIN_USER} from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
